16710f33195af6575ff26af46089f0450770af95
정유진 <yujin3504@gmail.com>
2024-12-11 17:20:40 +0900

    feat #4 - Weather Theme 모델 및 관리 메서드

---
+++ WeatherApp/Model/WeatherTheme.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherTheme.swift
+//  WeatherApp
+//
+//  Created by 정유진 on 12/10/24.
+//
+
+import UIKit
+
+struct WeatherTheme {
+    let backgroundColor: UIColor
+    let fontColor: UIColor
+    let pointColor1: UIColor
+    let pointColor2: UIColor
+    let animationName: String
+}
---
+++ WeatherApp/Service/WeatherThemeManager.swift
@@ -0,0 +1,82 @@
+//
+//  WeatherThemeManager.swift
+//  WeatherApp
+//
+//  Created by 정유진 on 12/10/24.
+//
+
+import Foundation
+
+final class WeatherThemeManager {
+    static let shared = WeatherThemeManager() // 싱글톤 패턴
+    
+    private init() {}
+    
+    func updateTheme(for weather: String) -> WeatherTheme {
+        switch weather {
+        case "sunny":
+            return WeatherTheme(
+                backgroundColor: .sunnyBackground,
+                fontColor: .sunnyFont,
+                pointColor1: .sunnyPoint1,
+                pointColor2: .sunnyPoint2,
+                animationName: "sunnyAnimation"
+            )
+            
+        case "cloudy":
+            return WeatherTheme(
+                backgroundColor: .cloudyBackground,
+                fontColor: .appDarkGray,
+                pointColor1: .cloudyPoint1,
+                pointColor2: .cloudyPoint2,
+                animationName: "cloudyAnimation"
+            )
+            
+        case "rainy":
+            return WeatherTheme(
+                backgroundColor: .rainyBackground,
+                fontColor: .appDarkGray,
+                pointColor1: .rainyPoint1,
+                pointColor2: .rainyPoint2,
+                animationName: "sunnyAnimation"
+            )
+            
+        case "snowy":
+            return WeatherTheme(
+                backgroundColor: .snowyBackground,
+                fontColor: .snowyFont,
+                pointColor1: .snowyPoint1,
+                pointColor2: .snowyPoint2,
+                animationName: "snowyAnimation"
+            )
+            
+        case "foggy":
+            return WeatherTheme(
+                backgroundColor: .foggyBackground,
+                fontColor: .appDarkGray,
+                pointColor1: .foggyPoint1,
+                pointColor2: .foggyPoint2,
+                animationName: "snowyAnimation"
+            )
+            
+        case "stormy":
+            return WeatherTheme(
+                backgroundColor: .stormyBackground,
+                fontColor: .white,
+                pointColor1: .stormyPoint1,
+                pointColor2: .stormyPoint2,
+                animationName: "sstormyAnimation"
+            )
+            
+        default:
+            return WeatherTheme(
+                backgroundColor: .sunnyBackground,
+                fontColor: .sunnyFont,
+                pointColor1: .sunnyPoint1,
+                pointColor2: .sunnyPoint2,
+                animationName: "sunnyAnimation"
+            )
+        }
+    }
+    
+}
