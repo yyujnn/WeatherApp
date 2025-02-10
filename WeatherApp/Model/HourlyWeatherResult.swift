3ede7d675f3f4951d51538a8a6ead755a0a1aa1a
정유진 <yujin3504@gmail.com>
2025-02-10 15:07:13 +0900

    fix #12 - HourlyResult 모델 수정

--- WeatherApp/Model/HourlyWeatherResult.swift
+++ WeatherApp/Model/HourlyWeatherResult.swift
@@ -12,11 +12,17 @@ struct HourlyWeatherResult: Codable {
 }
 
 struct HourlyWeather: Codable {
+    let dt: Int
     let dtTxt: String
     let main: WeatherMain
     let weather: [Weather]
     
+    var date: Date? {
+        return Date(timeIntervalSince1970: TimeInterval(dt))
+    }
+    
     enum CodingKeys: String, CodingKey {
+        case dt
         case dtTxt = "dt_txt"
         case main
         case weather
