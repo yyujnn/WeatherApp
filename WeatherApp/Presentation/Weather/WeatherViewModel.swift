9a94e5a29397de70cefa9d3ead167974377915d3
정유진 <yujin3504@gmail.com>
2025-02-11 15:09:20 +0900

    refactor #14 - ViewModel에서 데이터 로직 분리: API 호출 및 데이터 처리

--- WeatherApp/Presentation/Weather/WeatherViewModel.swift
+++ WeatherApp/Presentation/Weather/WeatherViewModel.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import Combine
+
+class WeatherViewModel {
+
+    @Published var currentWeather: CurrentWeatherResult?
+    @Published var hourlyWeather: HourlyWeatherResult?
+    @Published var errorMessage: String?
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    // API 호출
+    func fetchWeatherData(lat: Double, lon: Double) {
+        // 현재 날씨 데이터 가져오기
+        WeatherAPIManager.shared.fetchCurrentWeather(lat: lat, lon: lon)
+            .receive(on: DispatchQueue.main) // UI 업데이트를 위해 메인 스레드로 전환
+            .sink(receiveCompletion: { [weak self] completion in
+                if case .failure(let error) = completion {
+                    self?.errorMessage = "현재 날씨 불러오기 실패: \(error.localizedDescription)"
+                }
+            }, receiveValue: { [weak self] weatherData in
+                self?.currentWeather = weatherData
+            })
+            .store(in: &cancellables)
+        
+        // 시간별 날씨 데이터 가져오기
+        WeatherAPIManager.shared.fetchHourlyWeather(lat: lat, lon: lon)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                if case .failure(let error) = completion {
+                    self?.errorMessage = "시간별 날씨 불러오기 실패: \(error.localizedDescription)"
+                }
+            }, receiveValue: { [weak self] hourlyData in
+                self?.hourlyWeather = hourlyData
+            })
+            .store(in: &cancellables)
+    }
+}
