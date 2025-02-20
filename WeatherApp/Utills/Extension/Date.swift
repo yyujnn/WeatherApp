20eee7b9eb900a3a1adc6daf584845d8700fc8c2
정유진 <yujin3504@gmail.com>
2025-02-20 18:30:58 +0900

    refactor #19 - KST 변환 및 날짜 포맷을 위한 Date Extension 작성

--- WeatherApp/Utills/Extension/Date.swift
+++ WeatherApp/Utills/Extension/Date.swift
@@ -8,10 +8,40 @@
 import Foundation
 
 extension Date {
-    /// 현재 Date를 KST(Asia/Seoul) 시간으로 변환
-    func toKST() -> Date {
+    // 현재 Date를 KST(Asia/Seoul) 시간으로 변환
+    public func toKST() -> Date {
         let kstTimeZone = TimeZone(identifier: "Asia/Seoul")!
         let seconds = TimeInterval(kstTimeZone.secondsFromGMT(for: self))
         return self.addingTimeInterval(seconds)
     }
+    
+    public var summary: String {
+        return toString("yyyy-MM-dd HH:mm:ss")
+      }
+    
+    public var shortTime: String {
+        return toString("HH:mm")
+    }
+    
+    // MARK: - Date -> String
+    public func toString(_ dateFormat: String) -> String {
+        return DateFormatter
+            .convertToKoKR(dateFormat: dateFormat)
+            .string(from: self)
+    }
+}
+
+extension DateFormatter {
+    public static func convertToKoKR(dateFormat: String) -> DateFormatter {
+        let dateFormatter = createKoKRFormatter()
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter
+    }
+}
+
+private func createKoKRFormatter() -> DateFormatter {
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "ko_KR")
+    dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+    return dateFormatter
 }
