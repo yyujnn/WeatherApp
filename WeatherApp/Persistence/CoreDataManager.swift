afdd5b40e05f14ef17e1adc92b994af190b6945e
정유진 <yujin3504@gmail.com>
2025-03-29 20:19:27 +0900

    fix #30 - persistentContainer, saveSearchQuery

--- WeatherApp/Persistence/CoreDataManager.swift
+++ WeatherApp/Persistence/CoreDataManager.swift
@@ -5,25 +5,34 @@
 //  Created by 정유진 on 3/20/25.
 //
 
-import Foundation
+import UIKit
 import CoreData
 
 class CoreDataManager {
     static let shared = CoreDataManager()
     
-    let persistentContainer: NSPersistentContainer
+    var persistentContainer : NSPersistentContainer? {
+        (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    }
     
-    private init() {
-        persistentContainer = NSPersistentContainer(name: "WeatherApp")
-        persistentContainer.loadPersistentStores { _, error in
-            if let error = error{
-                fatalError("CoreData 로드 실패: \(error)")
-            }
+    // MARK: - 검색어 저장
+    func saveSearchQuery(_ query: String) {
+        guard let context = self.persistentContainer?.viewContext else { return }
+        
+        let newSearch = RecentSearch(context: context)
+        newSearch.query = query
+        newSearch.timestamp = Date()
+        
+        do {
+            try context.save()
+        } catch {
+            print("CoreData 저장 오류: \(error.localizedDescription)")
         }
     }
     
-    var context: NSManagedObjectContext {
-        return persistentContainer.viewContext
+    // MARK: - 최근 검색어 가져오기
+    func fetchRecentSearches() {
+        
     }
     
 }
